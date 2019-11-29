f0193d4331e6e6869ce7ba64943047288c7ddecc
hamed pishdad <hamed.hpn332@gmail.com>
2019-11-29 19:49:54 +0330

    part 12 -- navigate to a new page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:t1_mastering_fl/api/db_api.dart';
 import 'package:t1_mastering_fl/blocs/categories_bloc.dart';
 import 'package:t1_mastering_fl/models/Category.dart';
+import 'package:t1_mastering_fl/pages/selected_category_page.dart';
 import 'package:t1_mastering_fl/widgets/bloc_provider.dart';
 
 class HomePage extends StatelessWidget {
@@ -22,12 +23,20 @@ class HomePage extends StatelessWidget {
               return ListView.builder(
                   itemCount: categories.data.length,
                   itemBuilder: (context, index) {
-                    return Text(
-                      categories.data[index].name,
-                      style: TextStyle(fontSize: 24),
+                    return ListTile(
+                      onTap: () => Navigator.of(context).push(
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                SelectedCategoryPage()),
+                      ),
+                      title: Text(
+                        categories.data[index].name,
+                        style: TextStyle(fontSize: 24),
+                      ),
                     );
                   });
             }
+            return SizedBox();
           },
         ));
   }
