f915cd47762aa80bb070a806c80b4c81d2d6bdb9
hamed pishdad <hamed.hpn332@gmail.com>
2019-12-01 00:06:47 +0330

    part 19 -- installing rxdart

--- lib/blocs/cart_bloc.dart
+++ lib/blocs/cart_bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:rxdart/rxdart.dart';
 import 'package:t1_mastering_fl/models/cart.dart';
 import 'package:t1_mastering_fl/models/product.dart';
 import 'package:t1_mastering_fl/widgets/bloc_provider.dart';
@@ -7,7 +8,7 @@ import 'package:t1_mastering_fl/widgets/bloc_provider.dart';
 class CartBloc implements BlocBase {
   var cart = Cart();
 
-  final _productsController = StreamController<List<Product>>();
+  final _productsController = BehaviorSubject<List<Product>>();
   Sink<List<Product>> get _inProducts => _productsController.sink;
   Stream<List<Product>> get outProducts => _productsController.stream;
 
