2791d8cf4c2ce5e9beb2bb96e2f1c330e6e3eed8
hamed pishdad <hamed.hpn332@gmail.com>
2019-12-01 22:06:59 +0330

    part 21 -- supply whole app with cart bloc

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,12 @@ import 'package:t1_mastering_fl/blocs/categories_bloc.dart';
 import 'package:t1_mastering_fl/pages/home_page.dart';
 import 'package:t1_mastering_fl/widgets/bloc_provider.dart';
 
-void main() => runApp(MyApp());
+import 'blocs/cart_bloc.dart';
+
+void main() => runApp(BlocProvider<CartBloc>(
+      child: MyApp(),
+      bloc: CartBloc(),
+    ));
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
