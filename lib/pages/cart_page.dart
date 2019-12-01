e666323a2e65a3df2ca29c0782ce2ce15e069762
hamed pishdad <hamed.hpn332@gmail.com>
2019-12-01 23:10:47 +0330

    part 23 -- design the cart page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:t1_mastering_fl/blocs/cart_bloc.dart';
+import 'package:t1_mastering_fl/widgets/bloc_provider.dart';
 
 class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    final _cartBloc = BlocProvider.of<CartBloc>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('cart'),
+      ),
+      body: StreamBuilder(
+        stream: _cartBloc.outProducts,
+        initialData: [],
+        builder: (context, snapshot) {
+          if (snapshot.data.isNotEmpty) {
+            return ListView.builder(
+              itemCount: snapshot.data.lenght,
+              itemBuilder: (context, index) {
+                final product = snapshot.data[index];
+                return ListTile(
+                  title: Text(product.name),
+                  trailing: Text(product.amount.toString()),
+                );
+              },
+            );
+          } else {
+            return Center(
+              child: Text('no item in the cart.'),
+            );
+          }
+        },
       ),
     );
   }
